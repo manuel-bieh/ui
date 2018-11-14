87cc2b6e22ac30635010c002aac972252b0e4d55
Manuel Bieh <info@manuelbieh.de>
2018-11-15 00:47:33 +0100

    chore: allow adding class names to DataTable columns

--- src/DataTable/index.js
+++ src/DataTable/index.js
@@ -2,19 +2,20 @@
 import * as React from 'react';
 import css from './DataTable.module.css';
 
-type DataTableItem = [React.Element | string, React.Element | string];
+type DataTableItem = [React.Element<any> | string, React.Element<any> | string];
 
 type PropsT = {
     data: DataTableItem[],
+    columnClassNames?: [string, string],
 };
 
-const DataTable = ({ data }: PropsT) => (
+const DataTable = ({ data, columnClassNames }: PropsT) => (
     <table className={css.wrapper}>
         <tbody>
             {data.map((row, i) => (
                 <tr key={i}>
-                    <td>{row[0]}</td>
-                    <td>{row[1]}</td>
+                    <td className={(columnClassNames || [])[0]}>{row[0]}</td>
+                    <td className={(columnClassNames || [])[1]}>{row[1]}</td>
                 </tr>
             ))}
         </tbody>
