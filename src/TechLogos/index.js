9d721e3217410c6c71f288b62204ccd9f58e8165
Manuel Bieh <info@manuelbieh.de>
2018-11-21 01:17:18 +0100

    fix: alt texts for tech logos

--- src/TechLogos/index.js
+++ src/TechLogos/index.js
@@ -6,8 +6,8 @@ const TechLogos = () => (
     <section className={css.wrapper}>
         {techLogos.map((tech) => (
             <img
-                key={tech.name}
-                alt={`Logo: ${tech.name}`}
+                key={tech}
+                alt={`Logo: ${tech}`}
                 src={require(`../../assets/img/tech/${tech}`)}
             />
         ))}
