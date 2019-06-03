a7bfea8c16ed8a14d1b5cc0c0b55b79eb5f1eb8e
Manuel Bieh <info@manuelbieh.de>
2019-06-03 13:17:32 +0200

    feature: added datatable utils, fixed a few minor things

--- src/DataTable/index.tsx
+++ src/DataTable/index.tsx
@@ -1,20 +1,22 @@
 import * as React from 'react';
+import classNames from 'classnames';
 import css from './DataTable.module.css';
 
 type DataTableItem = [any, any];
 
 type PropsT = {
+    className?: string;
     data: DataTableItem[];
-    columnClassNames?: [string, string];
+    columnClassNames?: [string?, string?];
 };
 
-const DataTable = ({ data, columnClassNames }: PropsT) => (
-    <table className={css.wrapper}>
+const DataTable = ({ className, columnClassNames = [], data }: PropsT) => (
+    <table className={classNames(css.wrapper, className)}>
         <tbody>
             {data.map((row, i) => (
                 <tr key={i}>
-                    <td className={(columnClassNames || [])[0]}>{row[0]}</td>
-                    <td className={(columnClassNames || [])[1]}>{row[1]}</td>
+                    <td className={columnClassNames[0]}>{row[0]}</td>
+                    <td className={columnClassNames[1]}>{row[1]}</td>
                 </tr>
             ))}
         </tbody>
---
+++ src/DataTable/utils.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+
+type Labels = {
+    since: string;
+};
+
+export const workDataToDataTable = (items: any[], labels: Labels) =>
+    items.reduce((rows, item) => {
+        const { company, location, position, startDate, endDate } = item;
+        const start = endDate ? startDate.substring(0, 7) : labels.since;
+        const end = endDate
+            ? endDate.substring(0, 7)
+            : startDate.substring(0, 7);
+        const sep = endDate ? '–' : '';
+        rows.push([
+            <>
+                {start}
+                {sep}
+                <br />
+                {end}
+            </>,
+            <>
+                <strong>{position}</strong>
+                <br />
+                {company}, {location}
+            </>,
+        ]);
+        return rows;
+    }, []);
+
+export const educationDataToDataTable = (items: any[], labels: Labels) =>
+    items.reduce((rows, item) => {
+        const { institution, studyType, startDate, endDate } = item;
+        const start = endDate ? startDate.substring(0, 7) : labels.since;
+        const end = endDate
+            ? endDate.substring(0, 7)
+            : startDate.substring(0, 7);
+        const sep = endDate ? '–' : '';
+        rows.push([
+            <>
+                {start}
+                {sep}
+                <br />
+                {end}
+            </>,
+            <>
+                <strong>{studyType}</strong>
+                <br />
+                {institution}
+            </>,
+        ]);
+        return rows;
+    }, []);
